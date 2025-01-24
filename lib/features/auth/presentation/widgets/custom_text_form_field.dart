f3debed82544675fdaed332c6c89e3967dd74072
Shishir Rijal <ccrrizal0892@gmail.com>
2025-01-25 00:08:08 +0545

    add validator to custom text form field

--- lib/features/auth/presentation/widgets/custom_text_form_field.dart
+++ lib/features/auth/presentation/widgets/custom_text_form_field.dart
@@ -5,12 +5,13 @@ class CustomTextFormField extends StatelessWidget {
     required this.controller,
     required this.hintText,
     this.isPassword = false,
+    this.validator,
     super.key,
   });
   final TextEditingController controller;
   final String hintText;
   final bool isPassword;
-  // final Function(String?)? validator;
+  final Function(String?)? validator;
 
   @override
   Widget build(BuildContext context) {
@@ -18,13 +19,21 @@ class CustomTextFormField extends StatelessWidget {
       controller: controller,
       decoration: InputDecoration(
         hintText: hintText,
+        errorStyle: TextStyle(color: Colors.red, fontSize: 14),
       ),
       obscureText: isPassword,
       validator: (value) {
+        // If a validator is provided, use it
+        if (validator != null) {
+          return validator!(value);
+        }
+
+        // Else, use the default validator
         // check for empty field
         if (value!.isEmpty) {
           return '$hintText is missing!';
         }
+
         return null;
       },
     );
