313ce561b2cd09e826bee3f12c01ae9e16df5440
Shishir Rijal <ccrrizal0892@gmail.com>
2025-01-25 00:07:38 +0545

    update signup screen

--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:iwrite/core/theme/app_pallete.dart';
+import 'package:iwrite/features/auth/presentation/views/login_view.dart';
 import 'package:iwrite/features/auth/presentation/widgets/custom_button.dart';
 import 'package:iwrite/features/auth/presentation/widgets/custom_text_form_field.dart';
 
 class SignUpView extends StatefulWidget {
+  static route() => MaterialPageRoute(builder: (context) => SignUpView());
+
   const SignUpView({super.key});
 
   @override
@@ -25,63 +28,78 @@ class _SignUpViewState extends State<SignUpView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(),
         body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            spacing: 20,
-            children: [
-              Text("Sign Up.",
-                  style: TextStyle(
-                    fontSize: 50,
-                    color: AppPallete.whiteColor,
-                    fontWeight: FontWeight.bold,
-                  )),
-              SizedBox(height: 10),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                spacing: 20,
+                children: [
+                  Text("Sign Up.",
+                      style: TextStyle(
+                        fontSize: 50,
+                        color: AppPallete.whiteColor,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  SizedBox(height: 10),
 
-              // Name
-              CustomTextFormField(
-                  controller: _nameController, hintText: "Name"),
+                  // Name
+                  CustomTextFormField(
+                      controller: _nameController, hintText: "Name"),
 
-              // Email
-              CustomTextFormField(
-                  controller: _emailController, hintText: "Email"),
+                  // Email
+                  CustomTextFormField(
+                    controller: _emailController,
+                    hintText: "Email",
+                  ),
 
-              // Password
-              CustomTextFormField(
-                controller: _passwordController,
-                hintText: "Password",
-                isPassword: true,
+                  // Password
+                  CustomTextFormField(
+                    controller: _passwordController,
+                    hintText: "Password",
+                    isPassword: true,
+                  ),
+
+                  // Sign Up Button
+                  CustomButton(
+                    onPressed: () {
+                      _formKey.currentState!.validate();
+                      if (_formKey.currentState!.validate()) {
+                        submit();
+                      }
+                    },
+                    text: "Sign Up",
+                  ),
+
+                  // Already have an account?
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, LoginView.route());
+                    },
+                    child: RichText(
+                        text: TextSpan(
+                            text: "Already have an account? ",
+                            style: Theme.of(context).textTheme.titleMedium,
+                            children: [
+                          TextSpan(
+                              text: "Sign in",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(
+                                    color: AppPallete.gradient2,
+                                    fontWeight: FontWeight.bold,
+                                  )),
+                        ])),
+                  )
+                ],
               ),
-
-              // Sign Up Button
-              CustomButton(
-                onPressed: () {},
-                text: "Sign Up",
-              ),
-
-              // Already have an account?
-              RichText(
-                  text: TextSpan(
-                      text: "Already have an account? ",
-                      style: Theme.of(context).textTheme.titleMedium,
-                      children: [
-                    TextSpan(
-                        text: "Sign in",
-                        style:
-                            Theme.of(context).textTheme.titleMedium!.copyWith(
-                                  color: AppPallete.gradient2,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                  ]))
-            ],
+            ),
           ),
-        ),
-      ),
-    ));
+        ));
   }
 }
