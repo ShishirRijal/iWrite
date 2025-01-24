5294a7ee71aa80c8278e64ff596535b696a7bbda
Shishir Rijal <ccrrizal0892@gmail.com>
2025-01-25 00:31:58 +0545

    fix pod related error

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:iwrite/core/secrets/app_secrets.dart';
 import 'package:iwrite/core/theme/theme.dart';
 import 'package:iwrite/features/auth/presentation/views/login_view.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
+
   runApp(const MyApp());
 }
 
