a389c07f621940d22521599b7a8058f46a7e9022
Shishir Rijal <ccrrizal0892@gmail.com>
2025-01-25 00:56:19 +0545

    add auth remote data sources

---
+++ lib/features/auth/data/data_sources/auth_remote_data_source.dart
@@ -0,0 +1,52 @@
+import 'package:iwrite/core/error/exceptions.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class AuthRemoteDataSource {
+  Future<String> signUpWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+
+  Future<String> loginWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthRemoteDataSourceImpl extends AuthRemoteDataSource {
+  final SupabaseClient supabaseClient;
+
+  AuthRemoteDataSourceImpl({required this.supabaseClient});
+
+  @override
+  Future<String> loginWithEmailAndPassword(
+      {required String email, required String password}) {
+    // TODO: implement loginWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signUpWithEmailAndPassword(
+      {required String name,
+      required String email,
+      required String password}) async {
+    try {
+      final response = await supabaseClient.auth.signUp(
+        email: email,
+        password: password,
+        data: {
+          'name': name,
+        },
+      );
+
+      if (response.user == null) {
+        throw ServerException(message: "User is null");
+      }
+
+      return response.user!.id;
+    } catch (e) {
+      throw ServerException(message: e.toString());
+    }
+  }
+}
