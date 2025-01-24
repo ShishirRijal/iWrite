72b2142b307281f1e9fefc9c412967c60fcc5fb1
Shishir Rijal <ccrrizal0892@gmail.com>
2025-01-24 13:27:37 +0545

    add custom text form field

---
+++ lib/features/auth/presentation/widgets/custom_text_form_field.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({
+    required this.controller,
+    required this.hintText,
+    this.isPassword = false,
+    super.key,
+  });
+  final TextEditingController controller;
+  final String hintText;
+  final bool isPassword;
+  // final Function(String?)? validator;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
+      ),
+      obscureText: isPassword,
+      validator: (value) {
+        // check for empty field
+        if (value!.isEmpty) {
+          return '$hintText is missing!';
+        }
+      },
+    );
+  }
+}
