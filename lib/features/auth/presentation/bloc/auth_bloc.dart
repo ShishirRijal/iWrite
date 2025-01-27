456658f530b880bffc5a2dfa37edd1922abd9b0d
Shishir Rijal <ccrrizal0892@gmail.com>
2025-01-27 17:24:26 +0545

    add auth bloc

---
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:iwrite/features/auth/domain/usecases/user_sign_up.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final UserSignUp _userSignUp;
+
+  AuthBloc({required UserSignUp userSignUp})
+      : _userSignUp = userSignUp,
+        super(AuthInitial()) {
+    on<AuthSignUp>((event, emit) async {
+      final response = await _userSignUp(
+        SignUpParams(
+          name: event.name,
+          email: event.email,
+          password: event.password,
+        ),
+      );
+
+      response.fold((l) {
+        emit(AuthFailure(l.message));
+      }, (r) {
+        emit(AuthSuccess(r));
+      });
+    });
+  }
+}
