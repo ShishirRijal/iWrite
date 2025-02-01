803baa0f277fb4ec64b123f6b1bf8d2314551ed4
Shishir Rijal <ccrrizal0892@gmail.com>
2025-02-01 17:28:06 +0545

    add login usecase

---
+++ lib/features/auth/domain/usecases/user_login.dart
@@ -0,0 +1,28 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:iwrite/core/error/failures.dart';
+import 'package:iwrite/core/usecases/usecase.dart';
+import 'package:iwrite/features/auth/domain/entities/user.dart';
+import 'package:iwrite/features/auth/domain/repositories/auth_repository.dart';
+
+class UserLogin implements UseCase<User, LoginParams> {
+  final AuthRepository authRepository;
+  UserLogin(this.authRepository);
+
+  @override
+  Future<Either<Failure, User>> call(LoginParams params) async {
+    return await authRepository.loginWithEmailAndPassword(
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
+
+class LoginParams {
+  final String email;
+  final String password;
+
+  LoginParams({
+    required this.email,
+    required this.password,
+  });
+}
