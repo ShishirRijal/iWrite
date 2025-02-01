77b01df44c12a5e4f8afde268f515c45bc6f5c45
Shishir Rijal <ccrrizal0892@gmail.com>
2025-02-01 20:40:00 +0545

    create usecase to fetch current user

---
+++ lib/features/auth/domain/usecases/current_user.dart
@@ -0,0 +1,15 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:iwrite/core/error/failures.dart';
+import 'package:iwrite/core/usecases/usecase.dart';
+import 'package:iwrite/features/auth/domain/entities/user.dart';
+import 'package:iwrite/features/auth/domain/repositories/auth_repository.dart';
+
+class CurrentUser implements UseCase<User, NoParams> {
+  final AuthRepository authRepository;
+  CurrentUser(this.authRepository);
+
+  @override
+  Future<Either<Failure, User>> call(NoParams params) async {
+    return await authRepository.currentUser();
+  }
+}
