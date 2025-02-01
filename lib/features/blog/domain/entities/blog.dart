7e68ff16c9439739d305663163cd8cff43c40a59
Shishir Rijal <ccrrizal0892@gmail.com>
2025-02-02 01:18:56 +0545

    add blog entity

---
+++ lib/features/blog/domain/entities/blog.dart
@@ -0,0 +1,19 @@
+class Blog {
+  final String id;
+  final String userId;
+  final String title;
+  final String content;
+  final String imageUrl;
+  final List<String> tags;
+  final DateTime updatedAt;
+
+  Blog({
+    required this.id,
+    required this.userId,
+    required this.title,
+    required this.content,
+    required this.imageUrl,
+    required this.tags,
+    required this.updatedAt,
+  });
+}
