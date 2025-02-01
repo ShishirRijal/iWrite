078b7c153e02d6a97ddbfbb7860aff37cc0b563a
Shishir Rijal <ccrrizal0892@gmail.com>
2025-02-01 20:35:48 +0545

    add remote data source for getCurrentUserData

--- lib/features/auth/data/data_sources/auth_remote_data_source.dart
+++ lib/features/auth/data/data_sources/auth_remote_data_source.dart
@@ -3,6 +3,8 @@ import 'package:iwrite/features/auth/data/models/user_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract interface class AuthRemoteDataSource {
+  Session? get currentUserSession;
+
   Future<UserModel> signUpWithEmailAndPassword({
     required String name,
     required String email,
@@ -13,6 +15,8 @@ abstract interface class AuthRemoteDataSource {
     required String email,
     required String password,
   });
+
+  Future<UserModel?> getCurrentUserData();
 }
 
 class AuthRemoteDataSourceImpl extends AuthRemoteDataSource {
@@ -57,6 +61,26 @@ class AuthRemoteDataSourceImpl extends AuthRemoteDataSource {
       }
       print(response);
       return UserModel.fromJson(response.user!.toJson());
+    } catch (e) {
+      throw ServerException(message: e.toString());
+    }
+  }
+
+  @override
+  Session? get currentUserSession => supabaseClient.auth.currentSession;
+
+  @override
+  Future<UserModel?> getCurrentUserData() async {
+    try {
+      if (currentUserSession != null) {
+        final userData = await supabaseClient
+            .from('users')
+            .select()
+            .eq('id', currentUserSession!.user.id);
+
+        return UserModel.fromJson(userData.first);
+      }
+      return null;
     } catch (e) {
       throw ServerException(message: e.toString());
     }
